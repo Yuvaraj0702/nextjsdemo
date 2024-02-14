394c972f961ce23d25955cf0bf92926b374677cc
Yuvaraj0702 <yuvaraj408@gmail.com>
2024-02-14 16:38:32 +0800

    Add firestore code

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const handleForm = async () => {
+    const data = {
+      name: 'John snow',
+      house: 'Stark'
+    }
+    const { result, error } = await addData('users', 'user-id', data)
+
+    if (error) {
+      return console.log(error)
+    }
+  }
   return (
     <main className={styles.main}>
       <div className={styles.description}>
